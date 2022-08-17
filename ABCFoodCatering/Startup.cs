bc631180102ea0f6f3658403bc3f7c6bea7dcd23
VoidCodes <harodtai321@gmail.com>
2022-08-18 02:18:17 +0800

    change from my surface pc

--- ABCFoodCatering/Startup.cs
+++ ABCFoodCatering/Startup.cs
@@ -6,12 +6,15 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
 using ABCFoodCatering.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace ABCFoodCatering
 {
@@ -31,10 +34,17 @@ namespace ABCFoodCatering
 
             services.AddDbContext<ApplicationDbContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("ApplicationDbContext")));
+
+            // Add auth service
+            services.AddMvc(config =>
+            {
+                var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+                config.Filters.Add(new AuthorizeFilter(policy));
+            }).SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider serviceProvider)
         {
             if (env.IsDevelopment())
             {
@@ -49,6 +59,46 @@ namespace ABCFoodCatering
             app.UseStaticFiles();
             app.UseAuthentication();
             app.UseMvc();
+            CreateRoles(serviceProvider).Wait();
+        }
+
+        private async Task CreateRoles(IServiceProvider serviceProvider)
+        {
+            // Add roles
+            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var UserManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+            string[] roleNames = { "Admin", "Client", "User" };
+
+            IdentityResult roleResult;
+            foreach (var roleName in roleNames)
+            {
+                // Create roles and sync them to database
+                var roleExist = await RoleManager.RoleExistsAsync(roleName);
+                if (!roleExist)
+                {
+                    roleResult = await RoleManager.CreateAsync(new IdentityRole(roleName));
+                }
+
+                // Create Client User
+                var ClientUser = new IdentityUser
+                {
+                    UserName = Configuration.GetSection("UserSettings")["UserName"],
+                    Email = Configuration.GetSection("UserSettings")["UserEmail"]
+                };
+                string ClientUserPass = Configuration.GetSection("UserSettings")["UserPassword"];
+
+                var _ClientUserEmail = await UserManager.FindByEmailAsync(Configuration.GetSection("UserSettings")["UserEmail"]);
+                if (_ClientUserEmail == null)
+                {
+                    var createClientUser = await UserManager.CreateAsync(ClientUser, ClientUserPass);
+                    if (createClientUser.Succeeded)
+                    {
+                        await UserManager.AddToRoleAsync(ClientUser, "Client");
+                    }
+                }
+            }
         }
     }
+
+    //
 }
