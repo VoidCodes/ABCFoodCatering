762be75f283846990db719cd4f634509dd219aab
VoidCodes <harodtai321@gmail.com>
2022-08-24 16:54:12 +0800

    Minor changes and fixes

---
+++ ABCFoodCatering/Areas/Identity/Pages/Account/EditOrder.cshtml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ABCFoodCatering.Models;
+
+namespace ABCFoodCatering.Areas.Identity.Pages.Account
+{
+    public class EditOrderModel : PageModel
+    {
+        private readonly ABCFoodCatering.Models.ApplicationDbContext _context;
+
+        public EditOrderModel(ABCFoodCatering.Models.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Order Order { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Order = await _context.Order.FirstOrDefaultAsync(m => m.OrderID == id);
+
+            if (Order == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Order).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderExists(Order.OrderID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool OrderExists(int id)
+        {
+            return _context.Order.Any(e => e.OrderID == id);
+        }
+    }
+}
